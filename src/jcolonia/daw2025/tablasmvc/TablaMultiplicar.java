e630c9495f280125e92dd5856c1932e308a0edab
alejandurito123 <alevazarna2005@gmail.com>
2026-03-10 11:58:50 +0100

    He añadido la clase TablaMultiplicar 10/03/2026

---
+++ src/jcolonia/daw2025/tablasmvc/TablaMultiplicar.java
@@ -0,0 +1,31 @@
+package jcolonia.daw2025.tablasmvc;
+
+import java.util.List;
+
+public class TablaMultiplicar {
+
+	private int número;
+	private List<String> listaTextos;
+	
+	public TablaMultiplicar(int número) {
+		
+	}
+	
+	public void generarTabla() {
+		
+	}
+	
+	public List <String> toListaExportación(){
+		
+	}
+	
+	public List <String> toListaPantalla(){
+		
+	}
+	
+	@Override
+	public String toString() {
+		
+	}
+
+}
