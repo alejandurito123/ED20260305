961b42bf5649a157899edb31f1283b08adcc66a4
alejandurito123 <alevazarna2005@gmail.com>
2026-03-16 10:41:56 +0100

    En la clase VistaMenú he añadido el Javadoc

--- src/jcolonia/daw2025/tablasmvc/VistaMenú.java
+++ src/jcolonia/daw2025/tablasmvc/VistaMenú.java
@@ -2,8 +2,16 @@ package jcolonia.daw2025.tablasmvc;
 
 import java.util.Scanner;
 
+/**
+ * Gestiona la visualización y entrada de opciones del menú principal
+ * de la aplicación.
+ * @author Alejandro Vázquez Arnaiz
+ */
 public class VistaMenú {
 	
+	/**
+	 * Muestra por consola las opciones disponibles del menú principal.
+	 */
 	public static void mostrarOpciones() {
 		System.out.println("1) Mostrar la Tabla");
 		System.out.println("2) Exportar la tabla");
@@ -11,6 +19,10 @@ public class VistaMenú {
 		System.out.println("0) Salir");
 	}
 	
+	/**
+	 * Solicita al ususario que introduzca una opción del menú.
+	 * @return la opción elegida por el ususario como un número entero.
+	 */
 	public static int pedirOpción() {		
 		return VistaGeneral.pedirNúmero("Elija opción") ;
 		
